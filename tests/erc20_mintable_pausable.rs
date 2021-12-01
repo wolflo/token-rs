6e5a697a11d3adf6617eb32cc34f2b0c4efd102d
wolflo <33909953+wolflo@users.noreply.github.com>
2021-11-30 22:39:35 -0700

    Rough harness outline

--- tests/erc20_mintable_pausable.rs
+++ tests/erc20_mintable_pausable.rs
@@ -1,21 +1,25 @@
 use anyhow::Result;
 
-use crate::types::*;
+// use crate::types::*;
+use crate::*;
 use ethers::{prelude::*, utils::parse_ether};
 
 pub async fn test_name(ctx: Context) -> Result<()> {
+    println!("Testing name.");
     let name = &ctx.token.name().call().await?;
     assert_eq!(name, "Token");
     Ok(())
 }
 
 pub async fn test_symbol(ctx: Context) -> Result<()> {
+    println!("Testing symbol.");
     let symbol = &ctx.token.symbol().call().await?;
     assert_eq!(symbol, "TOK");
     Ok(())
 }
 
 pub async fn test_mint(ctx: Context) -> Result<()> {
+    println!("Testing mint.");
     let user = ctx.accts[1].address();
     let amt = U256::from(200);
 
@@ -27,6 +31,7 @@ pub async fn test_mint(ctx: Context) -> Result<()> {
 }
 
 pub async fn test_transfer(ctx: Context) -> Result<()> {
+    println!("Testing transfer.");
 
     let src = ctx.accts[1].address();
     let dst = ctx.accts[2].address();
@@ -34,9 +39,7 @@ pub async fn test_transfer(ctx: Context) -> Result<()> {
     let send_amt = parse_ether(U256::from(50))?;
 
     ctx.token.mint(src, mint_amt).send().await?;
-
-    //TODO: this is still coming from the connected client account
-    // ctx.token.transfer(dst, send_amt).from(src).send().await?;
+    ctx.token.transfer(dst, send_amt).from(src).send().await?;
 
     let bal_src = ctx.token.balance_of(src).call().await?;
     let bal_dst = ctx.token.balance_of(dst).call().await?;
